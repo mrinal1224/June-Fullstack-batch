00fd0357aa6ab981d64e1143cd453d1005ad7d22
mrinal1224 <iammrinalkc@gmail.com>
2024-06-27 21:39:39 +0530

    functions as Expression

--- Class-9 (execution Context and Hoisting)/function.js
+++ Class-9 (execution Context and Hoisting)/function.js
@@ -1,10 +1,31 @@
 // Functions
 
 // Syntax of a function
-function serveBeverage(quantity, drink){
-  console.log('I want ' + quantity  + ' ' + drink)
-} 
+// function serveBeverage(quantity, drink){
+//   console.log('I want ' + quantity  + ' ' + drink)
+// } 
 
-// Function invokation
-serveBeverage(3 , "coffee")
+// // Function invokation
+// serveBeverage(3 , "coffee")
+
+// Function as Expressions ( functions as first class citizens)
+
+// function sayHi(){}{
+//     console.log("HI")
+// }
+
+// sayHi()
+
+const sayHi = function(){
+    console.log('hi')
+}
+
+const serveBeverage = function(quantity , drink){
+    console.log('I want ' + quantity  + ' ' + drink)
+}
+
+
+sayHi()
+serveBeverage(3 , 'tea')
+
 
