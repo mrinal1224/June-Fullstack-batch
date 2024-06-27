bc235ba82e919ba211adabfb063aa9814ec7f001
mrinal1224 <iammrinalkc@gmail.com>
2024-06-27 21:27:54 +0530

    basic functions

---
+++ Class-9 (execution Context and Hoisting)/function.js
@@ -0,0 +1,10 @@
+// Functions
+
+// Syntax of a function
+function serveBeverage(quantity, drink){
+  console.log('I want ' + quantity  + ' ' + drink)
+} 
+
+// Function invokation
+serveBeverage(3 , "coffee")
+
