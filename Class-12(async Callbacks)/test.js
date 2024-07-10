527e518280fb8879583df616bb60c3d6d1f101c9
mrinal1224 <iammrinalkc@gmail.com>
2024-07-10 18:17:27 +0530

    push

--- Class-12(async Callbacks)/test.js
+++ Class-12(async Callbacks)/test.js
@@ -1,12 +1,19 @@
-setTimeout(function(){
-  for(let i=0 ; i<5 ; i++){
-    console.log(i)
-  }
-} , 5000)
+// setTimeout(function(){
+//   for(let i=0 ; i<5 ; i++){
+//     console.log(i)
+//   }
+// } , 5000)
 
 
-setInterval(function(){
-    for(let i=0 ; i<5 ; i++){
-             console.log(i)
-           }
-} , 3000)
+// setInterval(function(){
+//     for(let i=0 ; i<5 ; i++){
+//              console.log(i)
+//            }
+// } , 3000)
+
+
+for(let i=0 ; i<=5 ; i++){
+  setTimeout(function(){
+     console.log(i)
+  } , i*3000)
+}
