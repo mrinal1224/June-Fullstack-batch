73bfa16720e139ed85bf115e3fc6e15d7a9f9c5b
mrinal1224 <iammrinalkc@gmail.com>
2024-07-22 22:41:00 +0530

    push

--- Class-17 (Creating the Weather App)/script.js
+++ Class-17 (Creating the Weather App)/script.js
@@ -1,2 +1,16 @@
 
 
+
+
+async function fetchData(){
+    const endpoint = 'http://api.weatherapi.com/v1/current.json?key=35af7ff606db422880d141328231305&q=London&aqi=no'
+
+    const response = await fetch(endpoint)
+
+    const data = await response.json()
+
+    console.log(data)
+
+}
+
+fetchData()
