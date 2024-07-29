92d87d91a92ffa91be24e09a1401f11988544aea
mrinal1224 <iammrinalkc@gmail.com>
2024-07-29 22:39:55 +0530

    push

--- Class-18 (Kanban board-1)/script.js
+++ Class-18 (Kanban board-1)/script.js
@@ -1,10 +1,11 @@
 const addBtn = document.querySelector(".add-btn");
 
 const modalCont = document.querySelector(".modal-cont");
-const mainCont = document.querySelector('.main-cont')
-const textArea = document.querySelector('.textArea-cont')
-const allPriorityColors = document.querySelectorAll('.priority-color')
+const mainCont = document.querySelector(".main-cont");
+const textArea = document.querySelector(".textArea-cont");
+const allPriorityColors = document.querySelectorAll(".priority-color");
 modalCont.style.display = "none";
+let modalPriorityColor = 'lightpink'
 
 let addtaskFlag = false;
 
@@ -26,41 +27,35 @@ modalCont.addEventListener("keydown", function (e) {
   console.log(key);
 
   if (key === "Shift") {
-
-    let task = textArea.value
-    console.log(task)
-    createTicket(task);
+    let task = textArea.value;
+    console.log(task);
+    createTicket(task ,modalPriorityColor );
   }
 });
 
-function createTicket(TicketTask) {
-  let id = shortid()
+function createTicket(TicketTask , ticketColor) {
+  let id = shortid();
   let ticketCont = document.createElement("div");
   ticketCont.setAttribute("class", "ticket-cont");
 
-  ticketCont.innerHTML = ` <div class="ticket-color lightblue"></div>
+  ticketCont.innerHTML = ` <div class="ticket-color ${ticketColor}"></div>
     <div class="ticket-id">${id}</div>
     <div class="ticket-task">${TicketTask}</div>
     <div class="ticket-lock"><i class="fa-solid fa-lock"></i></div>`;
 
-    mainCont.appendChild(ticketCont)
-    modalCont.style.display = "none";
-
+  mainCont.appendChild(ticketCont);
+  modalCont.style.display = "none";
 }
 
+allPriorityColors.forEach(function (colorElem) {
+  colorElem.addEventListener("click", function () {
+    allPriorityColors.forEach(function (priorityColorElements) {
+      priorityColorElements.classList.remove("active");
+    });
+    colorElem.classList.add("active");
+    let colorSelected = colorElem.classList[1]
+     modalPriorityColor = colorSelected
+  });
+});
 
-allPriorityColors.forEach(function(colorElem){
-    colorElem.addEventListener('click' , function(){
-       allPriorityColors.forEach(function(priorityColorElements){
-       priorityColorElements.classList.remove('active')
-       })
-       colorElem.classList.add('active')
-       console.log(colorElem)
-    })
 
-  
-
-    
-})
-
-// Starting at 10:10 , Please be done with task addition and id genration
