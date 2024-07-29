4c774186bb43409b1286bd3345b0235b21f03bb3
mrinal1224 <iammrinalkc@gmail.com>
2024-07-29 21:33:49 +0530

    push

--- Class-18 (Kanban board-1)/script.js
+++ Class-18 (Kanban board-1)/script.js
@@ -1,17 +1,43 @@
-const addBtn = document.querySelector('.add-btn')
+const addBtn = document.querySelector(".add-btn");
 
-const modalCont = document.querySelector('.modal-cont')
-modalCont.style.display = 'none'
+const modalCont = document.querySelector(".modal-cont");
+const mainCont = document.querySelector('.main-cont')
+modalCont.style.display = "none";
 
-let addtaskFlag = false
+let addtaskFlag = false;
 
+// Modal toggle
+addBtn.addEventListener("click", function () {
+  addtaskFlag = !addtaskFlag;
 
-addBtn.addEventListener('click' , function(){
-   addtaskFlag = !addtaskFlag
+  if (addtaskFlag === true) {
+    modalCont.style.display = "flex";
+  } else {
+    modalCont.style.display = "none";
+  }
+});
 
-   if(addtaskFlag===true){
-    modalCont.style.display = 'flex'
-   }else{
-    modalCont.style.display = 'none'
-   }
-})
+/// Generating a Ticket
+
+modalCont.addEventListener("keydown", function (e) {
+  let key = e.key;
+  console.log(key);
+
+  if (key === "Shift") {
+    createTicket();
+  }
+});
+
+function createTicket() {
+  let ticketCont = document.createElement("div");
+  ticketCont.setAttribute("class", "ticket-cont");
+
+  ticketCont.innerHTML = ` <div class="ticket-color lightblue"></div>
+    <div class="ticket-id">12345</div>
+    <div class="ticket-task"></div>
+    <div class="ticket-lock"><i class="fa-solid fa-lock"></i></div>`;
+
+    mainCont.appendChild(ticketCont)
+    modalCont.style.display = "none";
+
+}
