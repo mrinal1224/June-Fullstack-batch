f4f9cb75ed807434ca1b1870ee0bce16941a86b2
mrinal1224 <iammrinalkc@gmail.com>
2024-08-16 21:57:01 +0530

    constrictor functions

--- Class-26 (OOPS-2)/index.js
+++ Class-26 (OOPS-2)/index.js
@@ -17,6 +17,14 @@ function Pizza(Toppings , Size , Crust){
   this.toppings = Toppings
   this.size = Size
   this.crust = Crust
+
+
+
+  this.describe = function(){
+    console.log(`This Pizza has ${this.toppings} ${this.size} ${this.crust}`)
+  }
+
+
 }
 
 let Pizza1 = new Pizza(['cheese' , 'tomatoe' , 'corn'] , 'Medium' , 'Thin')
@@ -24,8 +32,13 @@ let Pizza1 = new Pizza(['cheese' , 'tomatoe' , 'corn'] , 'Medium' , 'Thin')
 let Pizza2 = new Pizza(['onion' , 'mayo' , 'capsicum'] , 'Small' , 'Thick')
 
 
+
+
 console.log(Pizza1)
 console.log(Pizza2)
+
+Pizza1.describe()
+Pizza2.describe()
 
 
 
