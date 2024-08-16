5a3ffc51981d5c920d01fe33447d564bf8f47b76
mrinal1224 <iammrinalkc@gmail.com>
2024-08-16 23:00:11 +0530

    push

--- Class-26 (OOPS-2)/class.js
+++ Class-26 (OOPS-2)/class.js
@@ -3,16 +3,16 @@ class Pizza {
   static totalPizzasMade = 0
 
   constructor(toppings, size, crust) {
-    this.toppings = toppings;
-    this.size = size;
-    this.crust = crust;
+    this.#toppings = toppings;
+    this.#size = size;
+    this.#crust = crust;
     Pizza.totalPizzasMade++
 
   }
 
   describe() {
     console.log(
-      `This Pizza has ${this.toppings} with ${this.crust} crust and a ${this.size} Size`
+      `This Pizza has ${this.#toppings} with ${this.#crust} crust and a ${this.#size} Size`
     );
   }
  static calculateNumberOfPizzas(){
@@ -32,17 +32,13 @@ class StuffedPizza extends Pizza{
 
 const Pizza1 = new Pizza("cheese", "medium", "thin");
 
-const Pizza2 = new StuffedPizza('tomato' , 'large' , 'thick' , 'cheese and garlic')
+// const Pizza2 = new StuffedPizza('tomato' , 'large' , 'thick' , 'cheese and garlic')
 
-const Pizza3 = new StuffedPizza('tomato' , 'large' , 'thick' , 'cheese and garlic')
+// const Pizza3 = new StuffedPizza('tomato' , 'large' , 'thick' , 'cheese and garlic')
 
-Pizza3.describe()
 
-console.log(Pizza1);
 
-console.log(Pizza2)
 
-Pizza.calculateNumberOfPizzas()
 
 
 
