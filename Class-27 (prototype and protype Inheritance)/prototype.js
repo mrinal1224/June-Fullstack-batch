24ec43fe73f8278ce4ca334cd2c1e0a319157a77
mrinal1224 <iammrinalkc@gmail.com>
2024-08-19 22:27:42 +0530

    push

--- Class-27 (prototype and protype Inheritance)/prototype.js
+++ Class-27 (prototype and protype Inheritance)/prototype.js
@@ -4,12 +4,27 @@ Car.prototype.displayInfo = function () {
   );
 };
 
+
+// Car.prototype.__proto__.displayColor= function(){
+//     console.log(`Color of the Car is ${this.color}`)
+// }
+
+
+// let book = {
+//     name : 'The Great gatsby',
+//     publishedYear : 2001,
+// }
+
+// console.log(book)
+
+// book.displayColor()
+
 function Car(Name, Year, Color) {
   this.name = Name;
   this.year = Year;
   this.color = Color;
 
-  
+
 }
 
 let car1 = new Car("Mustang", 2021, "Red");
@@ -20,3 +35,10 @@ console.log(car2);
 
 car1.displayInfo();
 car2.displayInfo();
+
+car1.ShowYOM()
+
+
+console.log(car1.hasOwnProperty('color'))
+// car1.displayColor()
+// car2.displayColor()
