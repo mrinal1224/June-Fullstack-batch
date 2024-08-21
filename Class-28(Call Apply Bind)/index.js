16a966ac98288e2255909acf41665257912154d8
mrinal1224 <iammrinalkc@gmail.com>
2024-08-21 21:58:27 +0530

    call method done

--- Class-28(Call Apply Bind)/index.js
+++ Class-28(Call Apply Bind)/index.js
@@ -3,13 +3,18 @@ const person1 = {
   name: "john",
   age: 20,
 
-  printNameAndAge: function () {
-    console.log(`My name is ${this.name} and I am ${this.age} years old`);
+  printNameAndAge: function (location , favColor) {
+    console.log(`My name is ${this.name} and I am ${this.age} years old and i live in ${location} and My favuorite color is ${favColor}`);
   },
 };
 
 const person2={
     name : 'Mark',
+    age : 25,
+}
+
+const person3={
+    name : 'Adam',
     age : 25
 }
 
@@ -17,7 +22,9 @@ const person2={
 
 // The Object from whihc we are going to Borrow the method
 
-person1.printNameAndAge.call(person2)
+ person1.printNameAndAge.call(person3 , 'India' , 'Red')
+
+
 
 
 
